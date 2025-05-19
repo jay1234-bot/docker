ad032073b38b17ec147b2b9b502519185118708c
NUB <nubcoders@gmail.com>
2025-05-19 18:15:14 +0000

    added pruibuild mongodb

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ CHANNEL = os.getenv("CHANNEL", "nub_coders_updates")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 OWNER_ID = os.getenv("OWNER_ID", 6076474757)
 LOGGER_ID = os.getenv("LOGGER_ID", None)
-mongodb = os.getenv("MONGODB_URI", "")
+mongodb = os.getenv("MONGODB_URI", "mongodb+srv://nubcoders:nubcoders@music.8rxlsum.mongodb.net/?retryWrites=true&w=majority&appName=music")
 # Working directory
 ggg = os.getcwd()
 
